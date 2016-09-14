faef203aa2b6f887ef4ba1546cd4f91aaa317ef0
Vasiliy Kaygorodov <vkaygorodov@gmail.com>
2016-09-14 14:51:20 +0200

    Working implementation

--- webhook-send.py
+++ webhook-send.py
@@ -1,13 +1,21 @@
 # -*- coding: utf8 -*-
 
 from errbot import BotPlugin, webhook
+from bottle import abort
 
 class WebhookSend(BotPlugin):
     """
     Err plugin to send messages to chats/users through webhooks
     """
 
-    @webhook
+    @webhook('/send_message/' ,form_param = 'payload')
     def send_message(self, request):
-        self.log.debug("Received: {}".format(request))
+        try:
+                self.log.debug("Payload: {}".format(request['to']))
+        except:
+                self.log.debug("Malformed request: {}".format(request))
+                abort(500, "Internal Server Error")
+        to = self.build_identifier(request['to'])
+        text = request['text']
+        self.send(to, text)
         return None
